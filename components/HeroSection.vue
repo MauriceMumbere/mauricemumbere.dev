5e5dfaad1d1d4ce51afcf27c8a0f0234204356f2
maurice Mumbere <mauricendayango@gmail.com>
2025-06-28 21:00:34 +0100

    feat(components): add HeroSection component with introductory content

---
+++ components/HeroSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen px-4 md:px-12 lg:px-24">
+    <div
+      class="hero-content justify-between w-full flex-col lg:flex-row-reverse"
+    >
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+        class="w-[400px] rounded-lg shadow-2xl"
+      />
+      <div>
+        <span>Hello</span>
+        <h1 class="text-5xl font-bold">
+          <strong>I'm a fullstack web developer</strong>
+        </h1>
+        <p class="py-6 max-w-[25rem]">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <button class="btn btn-primary">Download resume</button>
+      </div>
+    </div>
+  </div>
+</template>
