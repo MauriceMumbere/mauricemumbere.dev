66b662a9942791425b91ac832161aa318a227e48
maurice Mumbere <mauricendayango@gmail.com>
2025-06-28 20:58:25 +0100

    feat(components): add Footer component with copyright notice

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer
+    class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4"
+  >
+    <aside>
+      <p>© maurice Mumbere</p>
+    </aside>
+  </footer>
+</template>
