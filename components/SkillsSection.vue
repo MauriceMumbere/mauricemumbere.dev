93e82fe2796c5f54c61b321e0c965771ebfdaa0a
maurice Mumbere <mauricendayango@gmail.com>
2025-06-28 20:57:39 +0100

    feat(components/organisms): add SkillsSection component with skill icons

---
+++ components/SkillsSection.vue
@@ -0,0 +1,101 @@
+<template>
+  <section
+    class="px-4 sm:px-8 md:px-16 lg:px-[100px] xl:px-[150px] py-10 md:py-16 lg:py-[100px]"
+  >
+    <h2 class="text-2xl md:text-3xl font-bold">My essentials skills</h2>
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-10 pt-8 md:pt-10"
+    >
+      <!-- Nuxt -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/nuxt-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Nuxt</h3>
+        </div>
+      </div>
+      <!-- Tailwindcss -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/tailwindcss-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Tailwindcss</h3>
+        </div>
+      </div>
+      <!-- Figma -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/figma-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Figma</h3>
+        </div>
+      </div>
+      <!-- Postgresql -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/postgresql-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Postgresql</h3>
+        </div>
+      </div>
+      <!-- Adonisjs -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/adonisjs-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Adonisjs</h3>
+        </div>
+      </div>
+      <!-- Wordpress -->
+      <div
+        class="flex justify-center items-center border border-gray-200 rounded-lg h-[120px] md:h-[150px]"
+      >
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <div>
+            <img
+              src="/assets/icons/wordpress-icon.svg"
+              alt=""
+              class="w-10 h-10 md:w-14 md:h-14"
+            />
+          </div>
+          <h3 class="text-lg md:text-xl">Wordpress</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
