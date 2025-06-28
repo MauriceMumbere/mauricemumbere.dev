0584e257c3cb682d6f4184c8540113df5f7881d2
maurice Mumbere <mauricendayango@gmail.com>
2025-06-28 21:00:46 +0100

    feat(components): add ProjectsSection component to showcase recent work

---
+++ components/ProjectsSection.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="px-4 md:px-12 lg:px-[150px] py-5 md:py-[20px]">
+    <h2 class="text-2xl font-bold">My Recent Work</h2>
+    <div class="py-6 md:py-10 space-y-4">
+      <div
+        class="flex flex-col md:flex-row gap-4 md:gap-8 border-b border-gray-200 py-5"
+      >
+        <div class="rounded-lg overflow-hidden flex-shrink-0 w-full md:w-auto">
+          <img
+            class="w-full h-40 md:h-45 md:w-45 object-cover"
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt=""
+          />
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold">Dashboard</h3>
+          <span class="block text-sm text-gray-500 mb-1">2025</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            impedit deserunt aliquam obcaecati libero maiores nemo reiciendis
+            magni, in maxime!
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col md:flex-row gap-4 md:gap-8 border-b border-gray-200 py-5"
+      >
+        <div class="rounded-lg overflow-hidden flex-shrink-0 w-full md:w-auto">
+          <img
+            class="w-full h-40 md:h-45 md:w-45 object-cover"
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt=""
+          />
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold">PomodoroFun</h3>
+          <span class="block text-sm text-gray-500 mb-1">2025</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            impedit deserunt aliquam obcaecati libero maiores nemo reiciendis
+            magni, in maxime!
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col md:flex-row gap-4 md:gap-8 border-b border-gray-200 py-5"
+      >
+        <div class="rounded-lg overflow-hidden flex-shrink-0 w-full md:w-auto">
+          <img
+            class="w-full h-40 md:h-45 md:w-45 object-cover"
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt=""
+          />
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold">Fylo</h3>
+          <span class="block text-sm text-gray-500 mb-1">2025</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            impedit deserunt aliquam obcaecati libero maiores nemo reiciendis
+            magni, in maxime!
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
