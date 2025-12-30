962b29a95841a4c5fc06d9cd69923c4ce2326bc3
maurice Mumbere <mauricendayango@gmail.com>
2025-12-30 16:02:43 +0100

    feat : creat footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,22 @@
 <template>
   <footer
-    class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4"
+    class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content lg:px-[250px] py-5"
   >
-    <aside>
-      <p>© maurice Mumbere</p>
-    </aside>
+    <div class="flex flex-col lg:flex-row justify-between w-full">
+      <p>© 2025 maurice Mumbere. Tous droits réservés.</p>
+      <div class="flex items-center space-x-2">
+        <span>Créé avec</span>
+        <img
+          src="/assets/icons/nuxt-icon.svg"
+          alt="Nuxt icon"
+          class="w-4 h-4 transition-transform duration-300 group-hover:scale-110"
+        />
+        <img
+          src="/assets/icons/tailwindcss-icon.svg"
+          alt="Tailwind CSS icon"
+          class="w-4 h-4 transition-transform duration-300 group-hover:scale-110"
+        />
+      </div>
+    </div>
   </footer>
 </template>
