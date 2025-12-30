ee7b9d18a3d13260a4f3c88b61ca41c9d8fd746e
maurice Mumbere <mauricendayango@gmail.com>
2025-12-30 16:03:40 +0100

    feat : Create HeroSection

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hero bg-base-200 min-h-screen px-4 md:px-12 lg:px-24">
+  <div class="hero bg-base-200 min-h-screen px-4 md:px-12 lg:px-[250px]">
     <div
       class="hero-content justify-between w-full flex-col lg:flex-row-reverse"
     >
@@ -7,17 +7,60 @@
         src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
         class="w-[400px] rounded-lg shadow-2xl"
       />
-      <div>
-        <span>Hello</span>
-        <h1 class="text-5xl font-bold">
-          <strong>I'm a fullstack web developer</strong>
+      <div class="space-y-5">
+        <h3
+          class="text-sm uppercase tracking-widest text-primary font-semibold"
+        >
+          Hello
+        </h3>
+        <h2 class="text-2xl">Developpeur web et mobile</h2>
+        <h1 class="text-4xl md:text-5xl font-bold leading-tight">
+          Maurice Mumbere
         </h1>
-        <p class="py-6 max-w-[25rem]">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
-        <button class="btn btn-primary">Download resume</button>
+        <div class="flex space-x-4">
+          <a
+            href="#"
+            class="flex items-center gap-2 border border-black/40 rounded-2xl p-[10px]"
+          >
+            <img
+              src="/assets/icons/tailwindcss-icon.svg"
+              alt="Tailwind CSS icon"
+              class="w-4 h-4 transition-transform duration-300 group-hover:scale-110"
+            />
+            Gmail
+          </a>
+          <a
+            href="#"
+            class="flex items-center gap-2 border border-black/40 rounded-2xl p-[10px]"
+          >
+            <img
+              src="/assets/icons/tailwindcss-icon.svg"
+              alt="Tailwind CSS icon"
+              class="w-4 h-4 transition-transform duration-300 group-hover:scale-110"
+            />
+            Linkedin
+          </a>
+          <a
+            href="#"
+            class="flex items-center gap-2 border border-black/40 rounded-2xl p-[10px]"
+          >
+            <img
+              src="/assets/icons/tailwindcss-icon.svg"
+              alt="Tailwind CSS icon"
+              class="w-4 h-4 transition-transform duration-300 group-hover:scale-110"
+            />
+            Github
+          </a>
+        </div>
+        <div>
+          <a
+            href="/assets/resume.pdf"
+            download
+            class="btn bg-green-300 w-full md:w-[400px] px-[10px] py-[20px]"
+          >
+            Download Resume
+          </a>
+        </div>
       </div>
     </div>
   </div>
